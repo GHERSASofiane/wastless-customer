7697cb8cf41ef3bdc0d1334d24227594cb37118f
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-13 23:05:13 +0200

    Add class RetailOffer

---
+++ src/pages/Class/RetailOffer.ts
@@ -0,0 +1,17 @@
+export class RetailOffer{
+
+    ProductName: String;
+    ProductDate: String;
+    ProductDescription: String;
+    ProductPicture: String;
+    UserName: String;
+    UserMail: String;
+    UserAdress: String;
+    
+    ProductId: number;
+    ProductPrice: number;
+    ProductStatus: number;
+    UserId: number; 
+    UserPhone: number;  
+
+}
