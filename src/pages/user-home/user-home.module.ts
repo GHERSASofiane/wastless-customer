106655f42a8dd80170021e5c77f24dcb13ec31a4
m.ghersa.s@gmail.com <m.ghersa.s@gmail.com>
2018-10-25 00:21:30 +0200

    Version 2.1

--- src/pages/user-home/user-home.module.ts
+++ src/pages/user-home/user-home.module.ts
@@ -1,86 +1,13 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
-import { User } from '../class/user';
-import { AuthProvider } from '../../providers/auth/auth';
-import { UserHomeServiceProvider } from '../../providers/user-home-service/user-home-service';
-import { HomePage } from '../home/home';
-import { Offer } from '../class/Offer';
-import { Reponse } from '../class/reponse';
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UserHomePage } from './user-home';
 
-
-
-@IonicPage()
-@Component({
-  selector: 'page-user-home',
-  templateUrl: 'user-home.html',
+@NgModule({
+  declarations: [
+    UserHomePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UserHomePage),
+  ],
 })
-export class UserHomePage {
-
-  user: User = new User("", "");
-  products: Offer[];
-  pages = [];
-
-  constructor(public navCtrl: NavController, private _auth: AuthProvider, private _userHome: UserHomeServiceProvider) {
-  }
-
-
-
-
-  ionViewCanEnter() {
-
-    if (this._auth.loggedIn()) {
-      /** 
-      const helper = new JwtHelperService();
-      let token = localStorage.getItem('token');                                    
-      this.user = helper.decodeToken(token);
-      */
-      this.user = this._auth.getUser();
-      this.offres();
-      return true;
-    }
-    else {
-
-      this.navCtrl.push(HomePage);
-      return false;
-    }
-
-  }
-
-  ionViewDidLeave() {
-    localStorage.removeItem('token');
-  }
-
-  ionViewWillLeave() {
-    localStorage.removeItem('token');
-  }
-
-
-
-  achats() {
-    alert("achats");
-  }
-
-  acheter() {
-    alert("acheter");
-  }
-
-  offres() {
-
-
-    this._userHome.getMyProducts().subscribe
-      (
-      res => {
-        let rep = new Reponse("", "");
-        rep = res;
-
-        this.products = rep.reponse;
-
-      },
-      err => console.log(err)
-      );
-
-  }
-
-
-
-}
+export class UserHomePageModule {}
--- src/pages/user-home/user-home.ts
+++ src/pages/user-home/user-home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { User } from '../class/user';
+import { AuthProvider } from '../../providers/auth/auth';
+import { UserHomeServiceProvider } from '../../providers/user-home-service/user-home-service';
+import { HomePage } from '../home/home';
+import { Offer } from '../class/Offer';
+import { Reponse } from '../class/reponse';
 
-/**
- * Generated class for the UserHomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -15,11 +16,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserHomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: User = new User("", "");
+  products: Offer[];
+  pages = [];
+
+  constructor(public navCtrl: NavController, private _auth: AuthProvider, private _userHome: UserHomeServiceProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserHomePage');
+
+
+
+  ionViewCanEnter() {
+
+    if (this._auth.loggedIn()) {
+      /** 
+      const helper = new JwtHelperService();
+      let token = localStorage.getItem('token');                                    
+      this.user = helper.decodeToken(token);
+      */
+      this.user = this._auth.getUser();
+      this.offres();
+      return true;
+    }
+    else {
+
+      this.navCtrl.push(HomePage);
+      return false;
+    }
+
   }
+
+  ionViewDidLeave() {
+    localStorage.removeItem('token');
+  }
+
+  ionViewWillLeave() {
+    localStorage.removeItem('token');
+  }
+
+
+
+  achats() {
+    alert("achats");
+  }
+
+  acheter() {
+    alert("acheter");
+  }
+
+  offres() {
+
+
+    this._userHome.getMyProducts().subscribe
+      (
+      res => {
+        let rep = new Reponse("", "");
+        rep = res;
+
+        this.products = rep.reponse;
+
+      },
+      err => console.log(err)
+      );
+
+  }
+
+
 
 }
